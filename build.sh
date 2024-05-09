0ee6b987d38db4ec45107da1b114bef36eb4a939
Mathias <mathias@projy.io>
2024-05-09 12:49:44 +0200

    mate for awesome

--- build.sh
+++ build.sh
@@ -15,12 +15,11 @@ KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 # this installs a package from fedora repos
 rpm-ostree uninstall libvirt-daemon-kvm zfs-fuse libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt
 rpm-ostree install /tmp/rpms/zfs/*.rpm pv tmux
-rpm-ostree install libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome
+rpm-ostree install libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit
 find /lib/modules/
 depmod -A ${KERNEL}
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
-
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
