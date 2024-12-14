7e488759271ba31b5d4629cc8b5a8dafbbbfd001
mathias <gumabe@bluefin>
2024-12-14 16:14:14 +0100

    dm stuff

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-#curl -o /etc/yum.repos.d/refi64-webapp-manager-fedora.repo "https://copr.fedorainfracloud.org/coprs/refi64/webapp-manager/repo/fedora-${RELEASE}/refi64-webapp-manager-fedora-${RELEASE}.repo"
-#curl -o /etc/yum.repos.d/solopasha-hyprland-fedora-41.repo "https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-41/solopasha-hyprland-fedora-41.repo"
-#rpm-ostree install hyprland
+curl -o /etc/yum.repos.d/refi64-webapp-manager-fedora.repo "https://copr.fedorainfracloud.org/coprs/refi64/webapp-manager/repo/fedora-${RELEASE}/refi64-webapp-manager-fedora-${RELEASE}.repo"
+curl -o /etc/yum.repos.d/solopasha-hyprland-fedora-41.repo "https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-41/solopasha-hyprland-fedora-41.repo"
+rpm-ostree install hyprland
 copr="cjuniorfox/hyprland-shell solopasha/hyprland   erikreider/SwayNotificationCenter errornointernet/packages tofik/nwg-shell tofik/sway"
 for i in ${copr}; do
     MAINTAINER="${i%%/*}"
@@ -32,8 +32,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 # this installs a package from fedora repos
 #rpm-ostree uninstall libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt
 #rpm-ostree install /tmp/rpms/zfs/*.rpm pv tmux
-rpm-ostree install libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit stow firefox jetbrains-mono-fonts python3-pip flameshot fedora-release-sway-atomic sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs kitty kitty-shell-integration kitty-terminfo alacritty inotify-tools inotify-tools-devel blueman hyprland hyprland-plugins wl-clipboard wlr-randr cliphist eww-git hypridle hyprlock hyprshot waypaper cmake cpio meson hyprland-shell-config wol-changer sway-audio-idle-inhibit at pyprland qt5ct qt6ct qt6-qtsvg swappy yad pipewire-utils pipewire-alsa pavucontrol  swaync nwg-look nwg-shell pcmanfm hyprpolkitagent xcompmgr copyq  driverctl incus-client gamescope
-#sddm-wayland-sway swaylock sway-contrib
+rpm-ostree install libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit stow firefox jetbrains-mono-fonts python3-pip flameshot fedora-release-sway-atomic sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs kitty kitty-shell-integration kitty-terminfo alacritty inotify-tools inotify-tools-devel blueman hyprland hyprland-plugins wl-clipboard wlr-randr cliphist eww-git hypridle hyprlock hyprshot waypaper cmake cpio meson hyprland-shell-config wol-changer sway-audio-idle-inhibit at pyprland qt5ct qt6ct qt6-qtsvg swappy yad pipewire-utils pipewire-alsa pavucontrol  swaync nwg-look nwg-shell pcmanfm hyprpolkitagent xcompmgr copyq  driverctl incus-client gamescope 
 
 #rpm-ostree install -y https://prerelease.keybase.io/keybase_amd64.rpm
 
--- justfile
+++ justfile
@@ -1,3 +1,7 @@
 build:
 	podman pull ghcr.io/ublue-os/bluefin-dx-nvidia:latest
 	podman build -t bluefin .
+
+pull_aurora:
+	podman pull ghcr.io/ublue-os/aurora-dx-nvidia:latest
+
