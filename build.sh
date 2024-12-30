cba222eba1fc19a6ee7be592fd69f38a2ec93b81
mathias <gumabe@bluefin>
2024-12-31 00:13:24 +0100

    lightdm

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 curl -o /etc/yum.repos.d/refi64-webapp-manager-fedora.repo "https://copr.fedorainfracloud.org/coprs/refi64/webapp-manager/repo/fedora-${RELEASE}/refi64-webapp-manager-fedora-${RELEASE}.repo"
 curl -o /etc/yum.repos.d/solopasha-hyprland-fedora-41.repo "https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-41/solopasha-hyprland-fedora-41.repo"
+curl -o /etc/yum.repos.d/ghostty.repo "https://copr.fedorainfracloud.org/coprs/pgdev/ghostty/repo/fedora-41/pgdev-ghostty-fedora-41.repo"
 rpm-ostree install hyprland
 copr="cjuniorfox/hyprland-shell solopasha/hyprland   erikreider/SwayNotificationCenter errornointernet/packages tofik/nwg-shell tofik/sway"
 for i in ${copr}; do
@@ -21,6 +22,10 @@ for i in ${copr}; do
     curl --output "/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:${MAINTAINER}:${REPOSITORY}.repo" --remote-name \
     "https://copr.fedorainfracloud.org/coprs/${MAINTAINER}/${REPOSITORY}/repo/fedora-${RELEASE}/${MAINTAINER}-${REPOSITORY}-fedora-${RELEASE}.repo"
 done
+
+#dnf copr enable pgdev/ghostty
+
+
 
 #rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
 #echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=299\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | tee /etc/yum.repos.d/firefoxpwa.repo
@@ -32,7 +37,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 # this installs a package from fedora repos
 #rpm-ostree uninstall libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt
 #rpm-ostree install /tmp/rpms/zfs/*.rpm pv tmux
-rpm-ostree install libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit stow firefox jetbrains-mono-fonts python3-pip flameshot fedora-release-sway-atomic sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs kitty kitty-shell-integration kitty-terminfo alacritty inotify-tools inotify-tools-devel blueman hyprland hyprland-plugins wl-clipboard wlr-randr cliphist eww-git hypridle hyprlock hyprshot waypaper cmake cpio meson hyprland-shell-config wol-changer sway-audio-idle-inhibit at pyprland qt5ct qt6ct qt6-qtsvg swappy yad pipewire-utils pipewire-alsa pavucontrol  swaync nwg-look nwg-shell pcmanfm hyprpolkitagent xcompmgr copyq  driverctl incus-client gamescope 
+rpm-ostree install libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit stow firefox jetbrains-mono-fonts python3-pip flameshot fedora-release-sway-atomic sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs kitty kitty-shell-integration kitty-terminfo alacritty inotify-tools inotify-tools-devel blueman hyprland hyprland-plugins wl-clipboard wlr-randr cliphist eww-git hypridle hyprlock hyprshot waypaper cmake cpio meson hyprland-shell-config wol-changer sway-audio-idle-inhibit at pyprland qt5ct qt6ct qt6-qtsvg swappy yad pipewire-utils pipewire-alsa pavucontrol  swaync nwg-look nwg-shell pcmanfm hyprpolkitagent xcompmgr copyq driverctl incus-client gamescope ghostty lightdm lightdm-settings
 
 #rpm-ostree install -y https://prerelease.keybase.io/keybase_amd64.rpm
 
--- justfile
+++ justfile
@@ -1,7 +1,10 @@
-build:
+pull:
 	podman pull ghcr.io/ublue-os/bluefin-dx-nvidia:latest
+build:
 	podman build -t bluefin .
 
+
+fullbuild: pull build
 pull_aurora:
 	podman pull ghcr.io/ublue-os/aurora-dx-nvidia:latest
 
