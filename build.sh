b591a872dd41f5616787802adf22c582bb97be2f
mathias <gumabe@bluefin>
2025-05-01 20:31:07 +0200

    update emacs

--- build.sh
+++ build.sh
@@ -16,35 +16,31 @@ KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 #curl -o /etc/yum.repos.d/ghostty.repo "https://copr.fedorainfracloud.org/coprs/pgdev/ghostty/repo/fedora-41/pgdev-ghostty-fedora-41.repo"
 #rpm-ostree install hyprland
 #copr="cjuniorfox/hyprland-shell solopasha/hyprland   erikreider/SwayNotificationCenter errornointernet/packages tofik/nwg-shell tofik/sway"
-#for i in ${copr}; do
-#    MAINTAINER="${i%%/*}"
-#    REPOSITORY="${i##*/}"
-#    curl --output "/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:${MAINTAINER}:${REPOSITORY}.repo" --remote-name \
-#    "https://copr.fedorainfracloud.org/coprs/${MAINTAINER}/${REPOSITORY}/repo/fedora-${RELEASE}/${MAINTAINER}-${REPOSITORY}-fedora-${RELEASE}.repo"
-#done
+copr="alternateved/bleeding-emacs"
+for i in ${copr}; do
+   MAINTAINER="${i%%/*}"
+   REPOSITORY="${i##*/}"
+   curl --output "/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:${MAINTAINER}:${REPOSITORY}.repo" --remote-name \
+   "https://copr.fedorainfracloud.org/coprs/${MAINTAINER}/${REPOSITORY}/repo/fedora-${RELEASE}/${MAINTAINER}-${REPOSITORY}-fedora-${RELEASE}.repo"
+done
 
 #dnf copr enable pgdev/ghostty
-
-
-
+dnf5 copr enable alternateved/bleeding-emacs
 #rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
 #echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=299\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | tee /etc/yum.repos.d/firefoxpwa.repo
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-
-
 # this installs a package from fedora repos
 #rpm-ostree uninstall libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt
 #rpm-ostree install /tmp/rpms/zfs/*.rpm pv tmux
 #rpm-ostree install libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit stow firefox jetbrains-mono-fonts python3-pip flameshot fedora-release-sway-atomic sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs kitty kitty-shell-integration kitty-terminfo alacritty inotify-tools inotify-tools-devel blueman hyprland hyprland-plugins wl-clipboard wlr-randr cliphist eww-git hypridle hyprlock hyprshot waypaper cmake cpio meson hyprland-shell-config wol-changer sway-audio-idle-inhibit at pyprland qt5ct qt6ct qt6-qtsvg swappy yad pipewire-utils pipewire-alsa pavucontrol  swaync nwg-look nwg-shell pcmanfm hyprpolkitagent xcompmgr copyq driverctl incus-client gamescope ghostty lightdm lightdm-settings
-rpm-ostree install libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit stow firefox jetbrains-mono-fonts python3-pip flameshot fedora-release-sway-atomic sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs kitty kitty-shell-integration kitty-terminfo alacritty inotify-tools inotify-tools-devel blueman wl-clipboard wlr-randr   cmake cpio meson at qt5ct qt6ct qt6-qtsvg swappy yad pipewire-utils pipewire-alsa pavucontrol pcmanfm  xcompmgr copyq driverctl incus-client gamescope lightdm lightdm-settings
+rpm-ostree install libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit stow firefox jetbrains-mono-fonts python3-pip flameshot fedora-release-sway-atomic sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs kitty kitty-shell-integration kitty-terminfo alacritty inotify-tools inotify-tools-devel blueman wl-clipboard wlr-randr cmake cpio meson at qt5ct qt6ct qt6-qtsvg swappy yad pipewire-utils pipewire-alsa pavucontrol pcmanfm xcompmgr copyq driverctl incus-client gamescope lightdm lightdm-settings gnome-session-xsession libffi-devel libtool libX11-devel libxml2-devel sddm-conf wxGTK-devel
 
 #rpm-ostree install -y https://prerelease.keybase.io/keybase_amd64.rpm
 
 # sddm-wayland-sway swaylock sway-contrib
-#rpm-ostree install 
-
+#rpm-ostree install
 
 #Install COPR packages from tofik/sway
 #rpm-ostree install sway-audio-idle-inhibit
--- justfile
+++ justfile
@@ -1,10 +1,11 @@
+fullbuild: pull_aurora build
+pull_aurora:
+	podman pull ghcr.io/ublue-os/aurora-dx-nvidia:latest
+
 pull:
 	podman pull ghcr.io/ublue-os/bluefin-dx-nvidia:latest
 build:
-	podman build -t bluefin .
+	podman build -t registry.local.alpha-awards.com/gumabe/bluefin .
 
 
-fullbuild: pull build
-pull_aurora:
-	podman pull ghcr.io/ublue-os/aurora-dx-nvidia:latest
 
