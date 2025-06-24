d5b97618ba26dfc152d65d3d95d02a46b9a3d7a7
mathias <gumabe@bluefin>
2025-06-24 07:22:01 +0200

    stgit

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,7 @@ set -ouex pipefail
 dnf5 -y copr enable ublue-os/staging
 #dnf5 -y remove zfs-fuse
 #dnf5 install /zfs/*.rpm
-dnf5 install -y tmux libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit stow firefox jetbrains-mono-fonts python3-pip flameshot fedora-release-sway-atomic sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs kitty kitty-shell-integration kitty-terminfo alacritty inotify-tools inotify-tools-devel blueman wl-clipboard wlr-randr cmake cpio meson at qt5ct qt6ct qt6-qtsvg swappy yad pipewire-utils pipewire-alsa pavucontrol swaync pcmanfm xcompmgr copyq driverctl incus-client gamescope lightdm lightdm-settings
+dnf5 install -y tmux libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit stow firefox jetbrains-mono-fonts python3-pip flameshot fedora-release-sway-atomic sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs kitty kitty-shell-integration kitty-terminfo alacritty inotify-tools inotify-tools-devel blueman wl-clipboard wlr-randr cmake cpio meson at qt5ct qt6ct qt6-qtsvg swappy yad pipewire-utils pipewire-alsa pavucontrol swaync pcmanfm xcompmgr copyq driverctl incus-client gamescope lightdm lightdm-settings gamescope gnome-session-xsession libffi-devel libtool libX11-devel libxml2-devel lightdm lightdm-greeter sddm-conf wxGTK-devel stgit
 
 # Use a COPR Example:
 #
