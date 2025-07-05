0bc3af58cbcb55e984f43a0a839db750b23a0c67
mathias <gumabe@bluefin>
2025-07-06 01:00:15 +0200

    base for mise

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,7 +16,7 @@ dnf5 -y copr enable ilyaz/LACT
 
 #dnf5 -y remove zfs-fuse
 #dnf5 install /zfs/*.rpm
-dnf5 install -y tmux libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit stow firefox jetbrains-mono-fonts python3-pip flameshot fedora-release-sway-atomic sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs kitty kitty-shell-integration kitty-terminfo alacritty inotify-tools inotify-tools-devel blueman wl-clipboard wlr-randr cmake cpio meson at qt5ct qt6ct qt6-qtsvg swappy yad pipewire-utils pipewire-alsa pavucontrol swaync pcmanfm xcompmgr copyq driverctl incus-client gamescope lightdm lightdm-settings gamescope gnome-session-xsession libffi-devel libtool libX11-devel libxml2-devel lightdm lightdm-greeter sddm-conf wxGTK-devel stgit lact
+dnf5 install -y tmux libvirt-daemon-kvm libvirt-daemon-driver-storage-zfs libvirt-daemon-driver-storage libvirt awesome mate-settings-daemon mate-control-center mate-polkit stow firefox jetbrains-mono-fonts python3-pip flameshot fedora-release-sway-atomic sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs kitty kitty-shell-integration kitty-terminfo alacritty inotify-tools inotify-tools-devel blueman wl-clipboard wlr-randr cmake cpio meson at qt5ct qt6ct qt6-qtsvg swappy yad pipewire-utils pipewire-alsa pavucontrol swaync pcmanfm xcompmgr copyq driverctl incus-client gamescope lightdm lightdm-settings gamescope gnome-session-xsession libffi-devel libtool libX11-devel libxml2-devel lightdm lightdm-greeter sddm-conf wxGTK-devel stgit lact ncurses-devel tig  sway-config-fedora hyprland
 
 # Use a COPR Example:
 #
