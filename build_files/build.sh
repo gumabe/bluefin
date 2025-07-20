b4a77abde931fac157756186edfb28405203d356
mathias <gumabe@bluefin>
2025-07-20 03:13:04 +0200

    vfio

--- build_files/build.sh
+++ build_files/build.sh
@@ -31,7 +31,7 @@ dnf5 -y copr enable swayfx/swayfx
 dnf5 install -y tmux fedora-release-xfce awesome qtile-extras fedora-release-sway-atomic \
   sway-config-fedora terminator neovim netcat emacs-gtk+x11 emacs libtool libffi-devel \
   libtool libX11-devel libxml2-devel pcmanfm inotify-tools inotify-tools-devel swayfx kitty kitty-shell-integration \
-  kitty-terminfo stgit lact ncurses-devel wxGTK-devel tig blueman flameshot cpio cmake meson at 
+  kitty-terminfo stgit lact ncurses-devel wxGTK-devel tig blueman flameshot cpio cmake meson at freetype-devel cairo-devel pango-devel wayland-devel libxkbcommon-devel harfbuzz
 
 # Use a COPR Example:
 #
